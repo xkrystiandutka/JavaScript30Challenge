94ce44699799328321ff766d90413a4b84766b27
Krystian <Krystian.Dutkait@gmail.com>
2021-11-25 12:22:38 +0100

    Added README & push again solution for exercise
    Incorrect order of commits on github, due to renaming an earlier commit.

---
+++ 07 - Array Cardio Day 2/README.txt
@@ -0,0 +1,2 @@
+Fix name commit
+Add initial files for day 76 -> Add initial files for day 7
--- 07 - Array Cardio Day 2/main.js
+++ 07 - Array Cardio Day 2/main.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 // ## Array Cardio Day 2
 
  const people = [{
@@ -41,26 +42,30 @@
  	}
 =======
  // ## Array Cardio Day 2
+=======
+// ## Array Cardio Day 2
+>>>>>>> bd35384 (Add solution for day 7)
 
  const people = [{
-         name: 'Wes',
-         year: 1988
-     },
-     {
-         name: 'Kait',
-         year: 1986
-     },
-     {
-         name: 'Irv',
-         year: 1970
-     },
-     {
-         name: 'Lux',
-         year: 2015
-     }
+ 		name: 'Wes',
+ 		year: 1988
+ 	},
+ 	{
+ 		name: 'Kait',
+ 		year: 1986
+ 	},
+ 	{
+ 		name: 'Irv',
+ 		year: 1970
+ 	},
+ 	{
+ 		name: 'Lux',
+ 		year: 2015
+ 	}
  ];
 
  const comments = [{
+<<<<<<< HEAD
          text: 'Love this!',
          id: 523423
      },
@@ -81,25 +86,53 @@
          id: 542328
      }
 >>>>>>> 29f194b (Add initial files for day 76)
+=======
+ 		text: 'Love this!',
+ 		id: 523423
+ 	},
+ 	{
+ 		text: 'Super good',
+ 		id: 823423
+ 	},
+ 	{
+ 		text: 'You are the best',
+ 		id: 2039842
+ 	},
+ 	{
+ 		text: 'Ramen is my fav food ever',
+ 		id: 123523
+ 	},
+ 	{
+ 		text: 'Nice Nice Nice!',
+ 		id: 542328
+ 	}
+>>>>>>> bd35384 (Add solution for day 7)
  ];
 
  // Some and Every Checks
  // Array.prototype.some() // is at least one person 19 or older?
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> bd35384 (Add solution for day 7)
  const firstAnswer = people.some(person => (new Date()).getFullYear() - person.year >= 19 ? true : false)
  console.log("1. ", firstAnswer);
 
  // Array.prototype.every() // is everyone 19 or older?
  const secondAnswer = people.every(person => (new Date()).getFullYear() - person.year >= 19);
  console.log("2. ", secondAnswer);
+<<<<<<< HEAD
 =======
  // Array.prototype.every() // is everyone 19 or older?
 >>>>>>> 29f194b (Add initial files for day 76)
+=======
+>>>>>>> bd35384 (Add solution for day 7)
 
  // Array.prototype.find()
  // Find is like filter, but instead returns just the one you are looking for
  // find the comment with the ID of 823423
 
+<<<<<<< HEAD
 <<<<<<< HEAD
  const thirdAnswer = comments.find(comment => comment.id === 823423);
  console.log('3. ', thirdAnswer);
@@ -118,3 +151,17 @@
  // Find the comment with this ID
  // delete the comment with the ID of 823423
 >>>>>>> 29f194b (Add initial files for day 76)
+=======
+ const thirdAnswer = comments.find(comment => comment.id === 823423);
+ console.log('3. ', thirdAnswer);
+
+ // Array.prototype.findIndex()
+ // Find the comment with this ID
+ // delete the comment with the ID of 823423
+
+ const index = comments.findIndex(comment => comment.id === 823423);
+ const fourthAnswer = [...comments]
+ fourthAnswer.splice(index, 1);
+
+ console.log('4. ', fourthAnswer);
+>>>>>>> bd35384 (Add solution for day 7)
