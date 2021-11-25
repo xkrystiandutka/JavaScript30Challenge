bd35384fcc4b5c6ff1a0fb601624067d092608df
Krystian <Krystian.Dutkait@gmail.com>
2021-11-25 11:48:48 +0100

    Add solution for day 7

--- 07 - Array Cardio Day 2/main.js
+++ 07 - Array Cardio Day 2/main.js
@@ -1,53 +1,67 @@
- // ## Array Cardio Day 2
+// ## Array Cardio Day 2
 
  const people = [{
-         name: 'Wes',
-         year: 1988
-     },
-     {
-         name: 'Kait',
-         year: 1986
-     },
-     {
-         name: 'Irv',
-         year: 1970
-     },
-     {
-         name: 'Lux',
-         year: 2015
-     }
+ 		name: 'Wes',
+ 		year: 1988
+ 	},
+ 	{
+ 		name: 'Kait',
+ 		year: 1986
+ 	},
+ 	{
+ 		name: 'Irv',
+ 		year: 1970
+ 	},
+ 	{
+ 		name: 'Lux',
+ 		year: 2015
+ 	}
  ];
 
  const comments = [{
-         text: 'Love this!',
-         id: 523423
-     },
-     {
-         text: 'Super good',
-         id: 823423
-     },
-     {
-         text: 'You are the best',
-         id: 2039842
-     },
-     {
-         text: 'Ramen is my fav food ever',
-         id: 123523
-     },
-     {
-         text: 'Nice Nice Nice!',
-         id: 542328
-     }
+ 		text: 'Love this!',
+ 		id: 523423
+ 	},
+ 	{
+ 		text: 'Super good',
+ 		id: 823423
+ 	},
+ 	{
+ 		text: 'You are the best',
+ 		id: 2039842
+ 	},
+ 	{
+ 		text: 'Ramen is my fav food ever',
+ 		id: 123523
+ 	},
+ 	{
+ 		text: 'Nice Nice Nice!',
+ 		id: 542328
+ 	}
  ];
 
  // Some and Every Checks
  // Array.prototype.some() // is at least one person 19 or older?
+ const firstAnswer = people.some(person => (new Date()).getFullYear() - person.year >= 19 ? true : false)
+ console.log("1. ", firstAnswer);
+
  // Array.prototype.every() // is everyone 19 or older?
+ const secondAnswer = people.every(person => (new Date()).getFullYear() - person.year >= 19);
+ console.log("2. ", secondAnswer);
 
  // Array.prototype.find()
  // Find is like filter, but instead returns just the one you are looking for
  // find the comment with the ID of 823423
 
+ const thirdAnswer = comments.find(comment => comment.id === 823423);
+ console.log('3. ', thirdAnswer);
+
  // Array.prototype.findIndex()
  // Find the comment with this ID
  // delete the comment with the ID of 823423
+
+ const index = comments.findIndex(comment => comment.id === 823423);
+ const fourthAnswer = [...comments]
+ fourthAnswer.splice(index, 1);
+
+ console.log('4. ', fourthAnswer);
