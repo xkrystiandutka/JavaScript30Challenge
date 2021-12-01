a7d1b8ec7797bfc0f4faabe4aea641bababeff48
Krystian <Krystian.Dutkait@gmail.com>
2021-12-01 12:31:01 +0100

    Add solution for day 13

--- 13 - Slide in on Scroll/main.js
+++ 13 - Slide in on Scroll/main.js
@@ -1,3 +1,9 @@
+/*
+ *Credit David Walsh (https://davidwalsh.name/javascript-debounce-function)
+ *
+ * ^that is why var is being used here
+ */
+
 function debounce(func, wait = 20, immediate = true) {
     var timeout;
     return function() {
@@ -13,3 +19,18 @@ function debounce(func, wait = 20, immediate = true) {
         if (callNow) func.apply(context, args);
     };
 }
+
+const images = document.querySelectorAll('.slide-in');
+
+const handleScroll = e => {
+    images.forEach(image => {
+        const slideInAt = (window.scrollY + window.innerHeight) - image.height / 2;
+        const imageBottomEdge = image.offsetTop + image.height;
+        const isHalfShown = slideInAt > image.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottomEdge;
+
+        isHalfShown && isNotScrolledPast ? image.classList.add('active') : image.classList.remove('active');
+    })
+}
+
+window.addEventListener('scroll', debounce(handleScroll));
